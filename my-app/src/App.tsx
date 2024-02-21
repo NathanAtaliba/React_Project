e26168b43dc9b90b746add2cca525895106b7d71
Nathan Santos Ataliba <nathan.s@inatel.br>
2024-02-21 16:59:50 -0300

    add postcss

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,11 @@
 export function App() {
   return (
+  //painel de tras
   //linha de cima(foto da atendente e nome da atendente)  
   //campo de troca de mensagens (conversa entre o chat e o cliente)
   //campo para digitação das mensagens (campo para digitação do usuario)
-  <div>
-
- 
-</div>
+  <div className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500">    
+Ola
+  </div>
   )
 }
