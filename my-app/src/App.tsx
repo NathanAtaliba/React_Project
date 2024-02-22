8d5aed12e2af7d01396a0ece71e40cfe7e3acbe6
Nathan Santos Ataliba <nathan.s@inatel.br>
2024-02-22 15:34:23 -0300

    editando arquivos e modificando codigos para identificar o tailwindcss

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,8 +4,9 @@ export function App() {
   //linha de cima(foto da atendente e nome da atendente)  
   //campo de troca de mensagens (conversa entre o chat e o cliente)
   //campo para digitação das mensagens (campo para digitação do usuario)
-  <div className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500">    
-Ola
+  <div className= 'bg-slate-400 text-center flex flex-row items-stretch space-x-4'>    
+    <div className='basis-1/4 p-1 text-cyan-400'>Foto</div> 
+    <div className='basis-1/2 p-1 text-cyan-400'>Atendente Virtual</div> 
   </div>
   )
 }
