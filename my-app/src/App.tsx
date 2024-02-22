c305f0395c40b6826f3305ac1bde450b71d02ecc
Nathan Santos Ataliba <nathan.s@inatel.br>
2024-02-22 16:00:36 -0300

    atendente

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,9 +4,12 @@ export function App() {
   //linha de cima(foto da atendente e nome da atendente)  
   //campo de troca de mensagens (conversa entre o chat e o cliente)
   //campo para digitação das mensagens (campo para digitação do usuario)
-  <div className= 'bg-slate-400 text-center flex flex-row items-stretch space-x-4'>    
-    <div className='basis-1/4 p-1 text-cyan-400'>Foto</div> 
-    <div className='basis-1/2 p-1 text-cyan-400'>Atendente Virtual</div> 
+  <div className= 'bg-slate-500 text-left flex flex-row items-stretch space-x-0'>    
+    <div className='basis-1/4 text-white'>
+      <img src="atendente.png" className="bg-white rounded-full w-10 "></img>
+    </div> 
+    <div className="basis-1/2 text-white font-bold text-left">Atendente Virtual</div> 
+
   </div>
   )
 }
