41e2fa9eff460650fb02d91c7c5017968097694f
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2024-02-22 23:27:20 -0300

    add interface

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -11,9 +11,10 @@ export function App() {
     </div> 
     <div className="p-10 basis-1/2 text-white font-bold text-left size-10 text-3xl">Atendente Virtual</div> 
   </div>
-  <div className="rounded-lg absolute flex flex-row w-full items-stretch gap-2 bg-slate-900 h-20 bottom-0 ">
-    <input className= "text-3xl rounded-lg bg-white text-left flex flex-row items-stretch text-black font-bold h-15 w-11/12" />
-    <button className="absolute bg-green-400 rounded-full h-18 w-1/12 right-0 top-0 h-20 ">=</button>
+  <div className="rounded-lg absolute flex flex-row w-full items-stretch gap-2 bg-transparent h-20 bottom-0 ">
+    <input type="text" 
+    className= "text-3xl rounded-lg bg-white text-left flex flex-row items-stretch text-black font-medium h-15 w-11/12" />
+    <button className="absolute bg-green-400 rounded-full h-18 w-1/12 right-0 top-0 h-20  placeholder:bg-green-800">Enviar</button>
   </div>
   </>  
 )
