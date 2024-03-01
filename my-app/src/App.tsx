429e68a46a9caeb3f138975667818d467c05ddf9
Nathan Santos Ataliba <nathan.s@inatel.br>
2024-03-01 16:28:12 -0300

    Modificações do frontend

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -12,25 +12,43 @@ export function App() {
 
   return ( 
   <>
-  <div className= "bg-slate-900 text-left flex flex-row items-stretch gap-2">    
+  <div className= "bg-slate-900 text-left flex flex-row items-stretch gap-2 relative">    
     <div className="basis-1/4 text-white p-5">
       <img src="atendente.png" className="bg-white rounded-full w-20"></img>
     </div> 
     <div className="p-10 basis-1/2 text-white font-bold text-left size-10 text-3xl">Atendente Virtual</div> 
   </div>
- 
-  <table className= "w-full">
-    <tr >
-      <td><NewQuestion/></td>
-    </tr>
-    <tr >
-      <td><NewResponse/></td>
-    </tr>
-  </table>
-  <div className="rounded-lg absolute flex flex-row w-full items-stretch gap-2 bg-transparent h-20 bottom-0 ">
+<div className="w-full overflow-y-scroll relative h-screen">
+    <NewQuestion/>
+    <NewResponse/>
+    <NewQuestion/>
+    <NewResponse/>
+    <NewQuestion/>
+    <NewResponse/>
+    <NewQuestion/>
+    <NewResponse/>
+    <NewQuestion/>
+    <NewResponse/>
+    <NewQuestion/>
+    <NewResponse/>
+    <NewQuestion/>
+    <NewResponse/>
+    <NewQuestion/>
+    <NewResponse/>
+    <NewQuestion/>
+    <NewResponse/>
+    <NewQuestion/>
+    <NewResponse/>
+    <NewQuestion/>
+    <NewResponse/>
+    <NewQuestion/>
+    <NewResponse/>
+
+</div>
+  <div className="rounded-lg flex flex-row w-full items-stretch gap-2 bg-transparent bottom-0 h-20 relative">
     <input type="text" 
-    className= "p-5 text-3xl rounded-full bg-slate-900 flex flex-row items-stretch text-white font-medium h-15 w-11/12 whitespace-break-spaces" placeholder="Mensagem" />
-    <button className="absolute bg-green-400 rounded-full h-18 w-1/12 right-0 top-0 h-20  placeholder:bg-green-800">Enviar</button>
+    className= "p-5 text-3xl rounded-full bg-slate-900 flex flex-row items-stretch text-white font-medium h-15 w-11/12" placeholder="Mensagem" />
+    <button className="relative bg-green-400 rounded-full h-18 w-1/12 h-20 right-0 placeholder:bg-green-800">Enviar</button>
   </div>
   </>  
 )
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -10,7 +10,7 @@ export default {
           sans: ['Inter','sans-serif']
         },
         backgroundImage: {
-          'wats': "url('/public/fundo.jpg')",
+          'wats': "url('/fundo.jpg')",
         }
       }
   },
