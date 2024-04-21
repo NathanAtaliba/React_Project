b30a7a2b430f07b470e8f7ed01c51b34b5223fd3
NathanAtaliba <nathanataliba28@gmail.com>
2024-04-21 16:39:44 -0300

    remove user

--- my-app/backend/src/routes.js
+++ my-app/backend/src/routes.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { getChat, getChats, createChat, deleteChat, updateChat, getResponse } from './controllers/chatController.js';
-import { getUser, createUser, deleteUser, updateUser } from './controllers/userController.js';
 
 const routes = Router();
 
@@ -10,13 +9,8 @@ routes.post('/chats/user', getChats);
 routes.post('/chat/create', createChat);
 routes.delete('/chat/delete/:id', deleteChat);
 routes.put('/chat/update/:id', updateChat);
-
 routes.post('/send/question', getResponse);
 
-//ROUTES User
-routes.get('/user/login', getUser);
-routes.post('/user/create', createUser);
-routes.delete('/user/delete', deleteUser);
-routes.put('/user/update', updateUser);
+
 
 export default routes;
